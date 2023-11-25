df8261d447fe64a7ec4e216507cc228d24ddb88a
raz0red <raz0red@gmail.com>
2023-11-24 22:00:50 -0800

    ProSystem initial

---
+++ prosystem-libretro-update/core/switchkeypad.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#include "switchkeypad.h"
+
+//#define  USE_SERIAL_CHECK_CARD
+
+
+enum  drv_key_bit{
+	BIT_SEL_KEY    =    1<<0,
+	BIT_REST_KEY   =    1<<1,
+	BIT_BW_COLOR   =    1<<2,
+	BIT_1P_AB      =    1<<3,
+	BIT_2P_AB      =    1<<4,
+	BIT_1609_403   =    1<<5,
+
+	BIT_CARD_DET   =    1<<6,
+};
+
+static int          threadcheck_run;
+static int          atari_card_det;
+
+int get_atari_card_state()
+{
+	return atari_card_det;
+}
+
+static int get_switchpad_state()
+{
+    FILE *fp = NULL;
+    int ret;
+
+    fp = fopen(SWITCHKEY_STATE, "r");
+    if (!fp) {
+        //printf("can't open %s\n", SPK_STATE);
+        return -1;
+    }
+    if (fscanf(fp, "%d", &ret)) {
+        fclose(fp);
+        fp = NULL;
+        return ret;
+    }
+    fclose(fp);
+    fp = NULL;
+    return -1;
+}
+
+int switchpad_update()
+{
+	int           curkey,report;
+
+	if(0 == atari_card_det)
+		return 0;
+	
+	curkey = get_switchpad_state();
+	
+	report = 0;
+	
+	if(BIT_SEL_KEY & curkey)
+		report |= SW_KEY_SEL;
+	
+	if(BIT_REST_KEY & curkey)
+		report |= SW_KEY_RST;
+	
+	if(BIT_BW_COLOR & curkey)
+		report |= SW_KEY_BW;
+	else
+		report |= SW_KEY_COLOR;
+	
+	if(BIT_1P_AB & curkey)
+		report |= SW_KEY_1P_B;
+	else
+		report |= SW_KEY_1P_A;
+	
+	if(BIT_2P_AB & curkey)
+		report |= SW_KEY_2P_B;
+	else
+		report |= SW_KEY_2P_A;
+
+	if(BIT_1609_403 & curkey) 
+		report |= SW_KEY_16_9;
+	else
+		report |= SW_KEY_4_3;
+	
+	if(BIT_CARD_DET & curkey) 
+		atari_card_det = 1;
+	else
+		atari_card_det = 0;
+
+	//printf("--%s: report = %#x \n",__func__,curkey);
+    return report;
+}
+
+//------------------------------------------
+
+int switchpad_init()
+{
+	atari_card_det = 1;
+	return 0;
+}
+
+void switchpad_deinit()
+{
+}
+
+
---
+++ prosystem-libretro-update/core/switchkeypad.h
@@ -0,0 +1,67 @@
+#ifndef  __SWITCHKEY_H__
+#define  __SWITCHKEY_H__
+
+#define SWITCHKEY_STATE                 "/sys/devices/platform/gpio-keys-polled/keys"
+
+enum  {
+	ASPECT_NONE=0,
+	ASPECT_16_9,
+	ASPECT_4_3,
+};
+
+/*
+ * refer to retroarch: aspectratio_lut[]
+*/
+enum aspect_ratio
+{
+   ASPECT_RATIO_4_3 = 0,
+   ASPECT_RATIO_16_9,
+   ASPECT_RATIO_16_10,
+   ASPECT_RATIO_16_15,
+   ASPECT_RATIO_21_9,
+   ASPECT_RATIO_1_1,
+   ASPECT_RATIO_2_1,
+   ASPECT_RATIO_3_2,
+   ASPECT_RATIO_3_4,
+   ASPECT_RATIO_4_1,
+   ASPECT_RATIO_4_4,
+   ASPECT_RATIO_5_4,
+   ASPECT_RATIO_6_5,
+   ASPECT_RATIO_7_9,
+   ASPECT_RATIO_8_3,
+   ASPECT_RATIO_8_7,
+   ASPECT_RATIO_19_12,
+   ASPECT_RATIO_19_14,
+   ASPECT_RATIO_30_17,
+   ASPECT_RATIO_32_9,
+   ASPECT_RATIO_CONFIG,
+   ASPECT_RATIO_SQUARE,
+   ASPECT_RATIO_CORE,
+   ASPECT_RATIO_CUSTOM,
+
+   ASPECT_RATIO_END
+};
+
+
+enum  {
+	SW_KEY_SEL     =    1<<0,
+	SW_KEY_RST     =    1<<1,
+	SW_KEY_BW      =    1<<2,
+	SW_KEY_COLOR   =    1<<3,
+	
+	SW_KEY_1P_A    =    1<<4,
+	SW_KEY_1P_B    =    1<<5,
+	SW_KEY_2P_A    =    1<<6,
+	SW_KEY_2P_B    =    1<<7,
+
+	SW_KEY_16_9    =    1<<8,
+	SW_KEY_4_3     =    1<<9,
+};
+
+
+extern int switchpad_update();
+extern int get_atari_card_state();
+extern int switchpad_init();
+extern void switchpad_deinit();
+
+#endif
